f032141877dfb5803b9a54236a7d432d797ab10e
CaptainM <37575000+hlmiao@users.noreply.github.com>
2021-01-14 23:33:23 +0800

    Update co_s3.py

--- co_s3.py
+++ co_s3.py
@@ -17,12 +17,12 @@ response = s3.list_objects_v2(Bucket='your-bucket-name')
 
 keys_to_delete = [{'Key': object['Key']}
                   for object in response['Contents']
-                  if object['LastModified'] > datetime.datetime.now().astimezone() - timedelta(days=7) #### check the day differ over 7 days
+                  if object['LastModified'] > datetime.datetime.now().astimezone() - timedelta(days=7) #### Check the day differ over 7 days
                      and ('crawler' in object['Key'] or 'Report' in object['Key']) ### Apply lifecycle rules with wildcards such as *crawler* and *Report* 
                  ]
 
-print(*keys_to_delete, sep="\n") ###Print is a function in Python 3.x
+print(*keys_to_delete, sep="\n") ### Print is a function in Python 3.x
 
-input("Confirm the Deleter files name and Press Enter to continue...")
+input("Confirm the Delete objects name and Press Enter to continue...")
 
 s3.delete_objects(Bucket='your-bucket-name', Delete={'Objects': keys_to_delete})
